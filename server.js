9b645ad55ed6bbf9c715fcf085c062d8a11e0e59
madnimba <jarintasneem248@gmail.com>
2023-07-13 09:51:19 +0600

    heroku rectify

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const path = require('path');
-const bodyParser = require('body-parser');
 
 app.get('/', (req, res) => {
     res.send("Hello World");
