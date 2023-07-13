f1be95a3d386943ae09fb2a3d37399cd33b53efb
madnimba <jarintasneem248@gmail.com>
2023-07-13 10:00:58 +0600

    homepage added

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
-    }   
+app.get('/', (req, res) => {   
+    res.sendFile(path.join(__dirname , '/views/index.html'));
+}   
 );
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-    }
-);
 
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)
+);
 
